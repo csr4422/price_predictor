0128661ac7b0517e27a0536065575820d79f4311
Chaitanya Raut <95222913+csr4422@users.noreply.github.com>
2024-10-04 20:46:07 +0530

    front.py
    implemented prophet for prediction

--- front.py
+++ front.py
@@ -36,5 +36,22 @@ def plot_raw_data():
     st.plotly_chart(fig)
 
 plot_raw_data()
+df_train = data[['Date','Close']]
+df_train=df_train.rename(columns={"Date":"ds","Close":"y"})
+m =Prophet()
+m.fit(df_train)
+future=m.make_future_dataframe(periods=period)
+forecast=m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_year} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
 
 
